a9905c53fd3a33e2d855b39d38299bde18c65876
Dennis Weber <dennis.weber@actico.com>
2018-06-29 15:23:26 +0200

    Add release-related buttons

--- src/info/dennisweber/modelingWorkflowEclipsePlugin/views/MainView.java
+++ src/info/dennisweber/modelingWorkflowEclipsePlugin/views/MainView.java
@@ -2,17 +2,16 @@ package info.dennisweber.modelingworkfloweclipseplugin.views;
 
 import java.time.Instant;
 
+import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
-import org.eclipse.swt.layout.RowData;
-import org.eclipse.swt.layout.RowLayout;
 import org.eclipse.swt.program.Program;
+import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Group;
-import org.eclipse.swt.widgets.Layout;
 import org.eclipse.swt.widgets.Link;
+import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Table;
 import org.eclipse.swt.widgets.TableColumn;
 import org.eclipse.swt.widgets.TableItem;
@@ -31,7 +30,7 @@ public class MainView extends ViewPart {
 		GridLayout mainLayout = new GridLayout();
 		mainLayout.numColumns = 1;
 		parent.setLayout(mainLayout);
-		
+
 		// Issues:
 		Group issueGroup = initIssueGroup(parent);
 		Table issueTable = initIssueTable(issueGroup);
@@ -41,22 +40,22 @@ public class MainView extends ViewPart {
 		// Release-Branches:
 		Group releaseBranchesGroup = initReleaseBranchesGroup(parent);
 		Table releaseBranchesTable = initReleaseBranchesTable(releaseBranchesGroup);
-		// TODO: "Create new active release branch" button
-		// TODO: "Build release from master" button
-		
+		Button createNewReleaseBranchButton = initCreateNewReleaseBranchButton(releaseBranchesGroup);
+		Button buildReleaseFromMasterButton = initBuildReleaseFromMasterButton(releaseBranchesGroup);
+
 		System.out.println(Instant.now().toString() + " Loaded.");
 	}
 
 	private Group initIssueGroup(Composite parent) {
 		Group issueGroup = new Group(parent, SWT.NONE);
 		issueGroup.setText("Active issues");
-		
+
 		// Layout:
 		GridLayout gl = new GridLayout();
 		gl.numColumns = 1;
 		issueGroup.setLayout(gl);
 		issueGroup.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
-		
+
 		return issueGroup;
 	}
 
@@ -69,7 +68,7 @@ public class MainView extends ViewPart {
 		data.heightHint = 200;
 		issueTable.setLayoutData(data);
 
-		String[] titles = { "ID", "Title", "Status", "Assignee", "Action"};
+		String[] titles = { "ID", "Title", "Status", "Assignee", "Action" };
 		for (int i = 0; i < titles.length; i++) {
 			TableColumn column = new TableColumn(issueTable, SWT.NONE);
 			column.setText(titles[i]);
@@ -92,7 +91,7 @@ public class MainView extends ViewPart {
 
 	private Link initViewAllIssuesLink(Group issueGroup) {
 		Link viewAllIssuesLink = new Link(issueGroup, SWT.NONE);
-		viewAllIssuesLink.setText("<a>View all Issues on GitHub</a>");
+		viewAllIssuesLink.setText("<a>View all Issues on Bitbucket</a>");
 		viewAllIssuesLink.addListener(SWT.Selection, event -> {
 			Program.launch("https://example.org"); // Launch the default browser
 			// TODO: This should link somewhere more useful.
@@ -111,11 +110,11 @@ public class MainView extends ViewPart {
 
 		return createIssueLink;
 	}
-	
+
 	private Group initReleaseBranchesGroup(Composite parent) {
 		Group releaseBranchesGroup = new Group(parent, SWT.NONE);
 		releaseBranchesGroup.setText("Active release branches:");
-		
+
 		// Layout:
 		GridLayout gl = new GridLayout();
 		gl.numColumns = 1;
@@ -124,7 +123,7 @@ public class MainView extends ViewPart {
 
 		return releaseBranchesGroup;
 	}
-	
+
 	private Table initReleaseBranchesTable(Composite parent) {
 		Table releaseBranchesTable = new Table(parent, SWT.MULTI | SWT.BORDER | SWT.FULL_SELECTION);
 		releaseBranchesTable.setLinesVisible(true);
@@ -134,7 +133,7 @@ public class MainView extends ViewPart {
 		data.heightHint = 200;
 		releaseBranchesTable.setLayoutData(data);
 
-		String[] titles = { "Release", "Last change", "Last tag", "Actions"};
+		String[] titles = { "Release", "Last change", "Last tag", "Actions" };
 		for (int i = 0; i < titles.length; i++) {
 			TableColumn column = new TableColumn(releaseBranchesTable, SWT.NONE);
 			column.setText(titles[i]);
@@ -152,5 +151,29 @@ public class MainView extends ViewPart {
 		}
 
 		return releaseBranchesTable;
+	}
+
+	private Button initCreateNewReleaseBranchButton(Composite parent) {
+		Button button = new Button(parent, SWT.PUSH);
+		button.setText("Create new active release Branch");
+		button.addListener(SWT.Selection, event -> {
+			Shell shell = this.getSite().getWorkbenchWindow().getShell();
+			MessageDialog.openError(shell, "Not implemented", "This feature is not implemented yet.");
+			// TODO: Make the button do something useful
+		});
+
+		return button;
+	}
+
+	private Button initBuildReleaseFromMasterButton(Composite parent) {
+		Button button = new Button(parent, SWT.PUSH);
+		button.setText("Build release from master");
+		button.addListener(SWT.Selection, event -> {
+			Shell shell = this.getSite().getWorkbenchWindow().getShell();
+			MessageDialog.openError(shell, "Not implemented", "This feature is not implemented yet.");
+			// TODO: Make the button do something useful
+		});
+
+		return button;
 	}
 }

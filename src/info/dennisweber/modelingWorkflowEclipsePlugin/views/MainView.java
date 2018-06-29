9dc62945a78b55ea126cbe1df8a23dd749f2ace4
Dennis Weber <dennis.weber@actico.com>
2018-06-29 11:34:42 +0200

    Add main view skeleton

---
+++ src/info/dennisweber/modelingWorkflowEclipsePlugin/views/MainView.java
@@ -0,0 +1,22 @@
+package info.dennisweber.modelingworkfloweclipseplugin.views;
+
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.ui.part.ViewPart;
+
+public class MainView extends ViewPart {
+	private Label label;
+
+	public MainView() {
+		super();
+	}
+
+	public void setFocus() {
+		label.setFocus();
+	}
+
+	public void createPartControl(Composite parent) {
+		label = new Label(parent, 0);
+		label.setText("Hello World");
+	}
+}

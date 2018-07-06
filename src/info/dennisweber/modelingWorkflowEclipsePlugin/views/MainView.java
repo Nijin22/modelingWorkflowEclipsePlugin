dae81c151f68793f481a0989292807a2e1aa4706
Dennis Weber <dennis.weber@actico.com>
2018-07-06 13:39:18 +0200

    Add selecting a active project + refactor master page in own class

--- src/info/dennisweber/modelingWorkflowEclipsePlugin/views/MainView.java
+++ src/info/dennisweber/modelingWorkflowEclipsePlugin/views/MainView.java
@@ -1,34 +1,27 @@
 package info.dennisweber.modelingworkfloweclipseplugin.views;
 
-import java.io.IOException;
-import java.time.Instant;
-import java.util.List;
-
-import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
 import org.eclipse.swt.layout.GridLayout;
-import org.eclipse.swt.program.Program;
-import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Combo;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Display;
-import org.eclipse.swt.widgets.Group;
-import org.eclipse.swt.widgets.Link;
+import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Shell;
-import org.eclipse.swt.widgets.Table;
-import org.eclipse.swt.widgets.TableColumn;
-import org.eclipse.swt.widgets.TableItem;
 import org.eclipse.ui.part.ViewPart;
 
 import info.dennisweber.modelingworkfloweclipseplugin.ConfigCache;
 import info.dennisweber.modelingworkfloweclipseplugin.dialogs.ConfigurationDialog;
-import info.dennisweber.modelingworkfloweclipseplugin.model.Issue;
-import info.dennisweber.modelingworkfloweclipseplugin.model.IssueStatus;
 import info.dennisweber.modelingworkfloweclipseplugin.model.JiraRestApi;
 
 public class MainView extends ViewPart {
-	ConfigCache configCache;
-	JiraRestApi jiraApi;
+	private ConfigCache configCache;
+	private JiraRestApi jiraApi;
+	private IProject selectedProject = null;
+	private MasterPage startseite;
+	Shell shell;
 
 	public MainView() {
 		super();
@@ -38,201 +31,71 @@ public class MainView extends ViewPart {
 	}
 
 	public void createPartControl(Composite parent) {
-		configCache = new ConfigCache();
-		if (configCache.isConfigured() == false) {
-			// Ensure the workflow tool is configured first
-			Shell shell = this.getSite().getWorkbenchWindow().getShell();
-			ConfigurationDialog configDialog = new ConfigurationDialog(shell, configCache);
-			configDialog.create();
-			configDialog.open(); // Open dialog and block until it is closed again
-		}
-
-		jiraApi = new JiraRestApi(configCache);
-
+		shell = this.getSite().getWorkbenchWindow().getShell();
 		GridLayout mainLayout = new GridLayout();
 		mainLayout.numColumns = 1;
 		parent.setLayout(mainLayout);
 
-		// Issues:
-		Group issueGroup = initIssueGroup(parent);
-		Table issueTable = initIssueTable(issueGroup);
-		Link issueLink = initCreateIssueLink(issueGroup);
-		Link viewAllIssuesLink = initViewAllIssuesLink(issueGroup);
+		initProjectSelectionCombo(parent);
 
-		// Release-Branches:
-		Group releaseBranchesGroup = initReleaseBranchesGroup(parent);
-		Table releaseBranchesTable = initReleaseBranchesTable(releaseBranchesGroup);
-		Button createNewReleaseBranchButton = initCreateNewReleaseBranchButton(releaseBranchesGroup);
-		Button buildReleaseFromMasterButton = initBuildReleaseFromMasterButton(releaseBranchesGroup);
+		if (selectedProject != null) {
 
-		// Config Button:
-		initConfigButton(parent);
-
-		System.out.println(Instant.now().toString() + " Main View loaded.");
-	}
-
-	private Group initIssueGroup(Composite parent) {
-		Group issueGroup = new Group(parent, SWT.NONE);
-		issueGroup.setText("Active issues");
-
-		// Layout:
-		GridLayout gl = new GridLayout();
-		gl.numColumns = 1;
-		issueGroup.setLayout(gl);
-		issueGroup.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
-
-		return issueGroup;
-	}
-
-	private Table initIssueTable(Composite parent) {
-		Table issueTable = new Table(parent, SWT.MULTI | SWT.BORDER | SWT.FULL_SELECTION);
-		issueTable.setLinesVisible(true);
-		issueTable.setHeaderVisible(true);
-
-		GridData data = new GridData(SWT.FILL, SWT.FILL, true, true);
-		data.heightHint = 200;
-		issueTable.setLayoutData(data);
-
-		String[] titles = { "ID", "Title", "Status", "Assignee", "Action" };
-		for (int i = 0; i < titles.length; i++) {
-			TableColumn column = new TableColumn(issueTable, SWT.NONE);
-			column.setText(titles[i]);
 		}
 
-		// Fill table in new thread
-		new Thread(() -> {
-			try {
-				List<Issue> issues = jiraApi.getIssues();
+	}
 
-				// Draw the update
-				Display.getDefault().syncExec(() -> {
-					for (Issue issue : issues) {
-						if (issue.getStatus() == IssueStatus.Done) {
-							continue; // Skip the issues which are already done.
+	private void initProjectSelectionCombo(Composite parent) {
+		Label label = new Label(parent, SWT.NONE);
+		label.setText("Active project:");
+
+		Combo combo = new Combo(parent, SWT.READ_ONLY);
+		for (IProject project : ResourcesPlugin.getWorkspace().getRoot().getProjects()) {
+			if (project.isOpen()) {
+				combo.add(project.getName());
+			}
+		}
+		combo.addSelectionListener(new SelectionListener() {
+
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				for (IProject project : ResourcesPlugin.getWorkspace().getRoot().getProjects()) {
+					if (project.isOpen() && project.getName().equals(combo.getText())) {
+						// Assume that users dont have two active projects with the same name.
+						if (!project.equals(selectedProject)) {
+							// project was changed
+							System.out.println("Switching active project to: " + project.getName());
+							selectedProject = project;
+
+							// Close old data, if it is there
+							if (startseite != null) {
+								startseite.dispose();
+								startseite = null;
+							}
+
+							configCache = new ConfigCache();
+							if (configCache.isConfigured() == false) {
+								// Ensure the workflow tool is configured first
+
+								ConfigurationDialog configDialog = new ConfigurationDialog(shell, configCache);
+								configDialog.create();
+								configDialog.open(); // Open dialog and block until it is closed again
+							}
+
+							jiraApi = new JiraRestApi(configCache);
+
+							startseite = new MasterPage(parent, jiraApi, configCache, shell);
+							startseite.init();
+							shell.redraw();
 						}
-						
-						TableItem item = new TableItem(issueTable, SWT.NONE);
-						item.setText(0, issue.getId());
-						item.setText(1, issue.getTitle());
-						item.setText(2, issue.getStatus().toString());
-						item.setText(3, issue.getAssignee());
-						item.setText(4, "This should be clickable");
 					}
-
-					// Adjust the width of columns
-					for (int i = 0; i < issueTable.getColumnCount(); i++) {
-						issueTable.getColumn(i).pack();
-					}
-				});
-			} catch (IOException e) {
-				System.out.println("Failed to update Issues.");
-				e.printStackTrace();
+				}
 			}
 
-		}).start();
-		;
-
-		return issueTable;
-	}
-
-	private Link initViewAllIssuesLink(Group issueGroup) {
-		Link viewAllIssuesLink = new Link(issueGroup, SWT.NONE);
-		viewAllIssuesLink.setText("<a>View all issues on Bitbucket</a>");
-		viewAllIssuesLink.addListener(SWT.Selection, event -> {
-			Program.launch("https://example.org"); // Launch the default browser
-			// TODO: This should link somewhere more useful.
+			@Override
+			public void widgetDefaultSelected(SelectionEvent e) {
+				// No text field in this combo - so this method shouldn't do anything.
+			}
 		});
-
-		return viewAllIssuesLink;
 	}
 
-	private Link initCreateIssueLink(Group issueGroup) {
-		Link createIssueLink = new Link(issueGroup, SWT.NONE);
-		createIssueLink.setText("<a>Create new issue</a>");
-		createIssueLink.addListener(SWT.Selection, event -> {
-			Program.launch("https://example.org"); // Launch the default browser
-			// TODO: This should link somewhere more useful.
-		});
-
-		return createIssueLink;
-	}
-
-	private Group initReleaseBranchesGroup(Composite parent) {
-		Group releaseBranchesGroup = new Group(parent, SWT.NONE);
-		releaseBranchesGroup.setText("Active release branches:");
-
-		// Layout:
-		GridLayout gl = new GridLayout();
-		gl.numColumns = 1;
-		releaseBranchesGroup.setLayout(gl);
-		releaseBranchesGroup.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
-
-		return releaseBranchesGroup;
-	}
-
-	private Table initReleaseBranchesTable(Composite parent) {
-		Table releaseBranchesTable = new Table(parent, SWT.MULTI | SWT.BORDER | SWT.FULL_SELECTION);
-		releaseBranchesTable.setLinesVisible(true);
-		releaseBranchesTable.setHeaderVisible(true);
-
-		GridData data = new GridData(SWT.FILL, SWT.FILL, true, true);
-		data.heightHint = 200;
-		releaseBranchesTable.setLayoutData(data);
-
-		String[] titles = { "Release", "Last change", "Last tag", "Actions" };
-		for (int i = 0; i < titles.length; i++) {
-			TableColumn column = new TableColumn(releaseBranchesTable, SWT.NONE);
-			column.setText(titles[i]);
-		}
-		int count = 2;
-		for (int i = 0; i < count; i++) {
-			TableItem item = new TableItem(releaseBranchesTable, SWT.NONE);
-			item.setText(0, Integer.toString(i));
-			item.setText(1, "Xh ago");
-			item.setText(2, "3.1.4");
-			item.setText(3, "This should be a button");
-		}
-		for (int i = 0; i < titles.length; i++) {
-			releaseBranchesTable.getColumn(i).pack();
-		}
-
-		return releaseBranchesTable;
-	}
-
-	private Button initCreateNewReleaseBranchButton(Composite parent) {
-		Button button = new Button(parent, SWT.PUSH);
-		button.setText("Create new active release branch");
-		button.addListener(SWT.Selection, event -> {
-			Shell shell = this.getSite().getWorkbenchWindow().getShell();
-			MessageDialog.openError(shell, "Not implemented", "This feature is not implemented yet.");
-			// TODO: Make the button do something useful
-		});
-
-		return button;
-	}
-
-	private Button initBuildReleaseFromMasterButton(Composite parent) {
-		Button button = new Button(parent, SWT.PUSH);
-		button.setText("Build release from master");
-		button.addListener(SWT.Selection, event -> {
-			Shell shell = this.getSite().getWorkbenchWindow().getShell();
-			MessageDialog.openError(shell, "Not implemented", "This feature is not implemented yet.");
-			// TODO: Make the button do something useful
-		});
-
-		return button;
-	}
-
-	private Button initConfigButton(Composite parent) {
-		Button button = new Button(parent, SWT.PUSH);
-		button.setText("Configuration");
-		button.addListener(SWT.Selection, event -> {
-			Shell shell = this.getSite().getWorkbenchWindow().getShell();
-			ConfigurationDialog configDialog = new ConfigurationDialog(shell, configCache);
-			configDialog.create();
-			configDialog.open(); // Open dialog and block until it is closed again
-		});
-
-		return button;
-	}
 }
